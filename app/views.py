ca6f0bb94530540bd32d7b352a91c2d387b05884
Muhammad Hanif <pocisite@gmail.com>
2017-11-22 16:32:18 +0700

    python3 version complete

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect
 from app import app, mysql
 from .forms import MyForm
 
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 def index():
     form = MyForm()
@@ -11,13 +11,13 @@ def index():
     
 
     if form.validate_on_submit():
-        print form.name.data
+        print(form.name.data)
         cursor.execute('INSERT INTO user (id, name) VALUES (NULL, \'%s\' )' % str(form.name.data))
         conn.commit()
         #This is stored procedure to call list of user
         cursor.execute('call test2()')
         data = cursor.fetchall()
-        print data
+        print(data)
         flash('REQUESTED DATA %s' % form.name.data)
         return str('You Input '+ form.name.data)
     return render_template('formku.html', title='DUmmy form', form=form)
